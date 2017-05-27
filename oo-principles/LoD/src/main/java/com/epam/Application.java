80cc7cb4abe004ad9b8790bba75a581e72cdda47
Yg0R2 <yg0r2@yahoo.com>
2017-05-27 12:17:36 +0200

    Refactor LoD

--- oo-principles/LoD/src/main/java/com/epam/Application.java
+++ oo-principles/LoD/src/main/java/com/epam/Application.java
@@ -7,17 +7,26 @@ import com.epam.domain.Paragraph;
 public class Application {
 
 	private void start() {
-		Document document = new DocumentCreator().createDocument();
+		Document document = (new DocumentCreator()).createDocument();
 		printDocument(document);
 	}
 
 	private void printDocument(Document document) {
 		for (Paragraph paragraph : document.getParagraphs()) {
-			for (Line line : paragraph.getLines()) {
-				System.out.println(line.getContent());
-			}
-			System.out.println();
+			printParagraph(paragraph);
 		}
+	}
+
+	private void printParagraph(Paragraph paragraph) {
+		for (Line line : paragraph.getLines()) {
+			printLine(line);
+		}
+
+		System.out.println();
+	}
+
+	private void printLine(Line line) {
+		System.out.println(line.getContent());
 	}
 
 	public static void main(String[] args) {

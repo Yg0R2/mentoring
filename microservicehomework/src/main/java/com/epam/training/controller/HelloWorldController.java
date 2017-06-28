30d6b2425b6283658b5076ffeb6cf3e06a5a61c3
tiborkovacs <Tibor_Kovacs1@epam.com>
2017-06-28 18:11:49 +0200

    Change package hierarchy

--- microservicehomework/src/main/java/com/epam/training/controller/HelloWorldController.java
+++ microservicehomework/src/main/java/com/epam/training/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.epam.controller;
+package com.epam.training.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
